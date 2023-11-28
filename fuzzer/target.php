57bc678312ad8deaca9238cf99d617aeaa62d9d6
JoshyPHP <s9e.dev@gmail.com>
2023-11-28 06:00:02 +0100

    Updated fuzzer

--- fuzzer/target.php
+++ fuzzer/target.php
@@ -5,7 +5,18 @@ require __DIR__ . '/../vendor/autoload.php';
 $fuzzer->setTarget(
 	function (string $input)
 	{
-		s9e\Bencode\Bencode::decode($input);
+		try
+		{
+			$decoded = s9e\Bencode\Bencode::decode($input);
+			if (s9e\Bencode\Bencode::encode($decoded) !== $input)
+			{
+				trigger_error('Does not match', E_USER_ERROR);
+			}
+		}
+		catch (s9e\Bencode\Exceptions\DecodingException $e)
+		{
+		}
+
 		s9e\Bencode\Bencode::decodeNonCompliant($input);
 	}
 );
